32f4fc119c41c4b341c65afbfccdff60f8f7cb98
Aleksandar Radulovic <aleksandar_radulovic@rocketmail.com>
2020-06-13 13:36:16 +0200

    Updates

---
+++ precache-manifest.eb77afa0133afa0fbab2bd3fd2c41896.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b7299f547e200f89b8c3286eec3a6a0",
+    "url": "/vast-ui-component/index.html"
+  },
+  {
+    "revision": "451b3c570d1bf7b527ce",
+    "url": "/vast-ui-component/static/css/2.2c0ec7c8.chunk.css"
+  },
+  {
+    "revision": "2a24e6b661a481768088",
+    "url": "/vast-ui-component/static/css/main.432e6f4e.chunk.css"
+  },
+  {
+    "revision": "451b3c570d1bf7b527ce",
+    "url": "/vast-ui-component/static/js/2.4d84ac0d.chunk.js"
+  },
+  {
+    "revision": "cf728d7b67769a93403fd921d7e1433a",
+    "url": "/vast-ui-component/static/js/2.4d84ac0d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a24e6b661a481768088",
+    "url": "/vast-ui-component/static/js/main.37e54558.chunk.js"
+  },
+  {
+    "revision": "b1e3be7ad825b9a3d1a9",
+    "url": "/vast-ui-component/static/js/runtime-main.13f1b660.js"
+  }
+]);
